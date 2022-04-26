bda8e01a9e2ed580f332e9f7462bf69a0dd0fd16
Sooriya Prakash <sooriyaprakashjp@gmail.com>
2022-04-26 18:11:12 +0530

    second commit

--- Main.java
+++ Main.java
@@ -3,6 +3,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("Sooriya got placed in TCS Digital");
+        System.out.println("Sooriya got placed in TCS Digital");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("If You want Briyani?");
+        String s=sc.next();
+        if(s.equalsIgnoreCase("yes"))
+        {
+            System.out.println("Eat Briyani");
+        }
+        else
+        {
+            System.out.println("Eat sambar rice");
+        }
     }
+
 }
